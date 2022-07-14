abaf52140ccd2359796bb4e6318d4b99eefb833c
sony-shah <82110724+sony-shah@users.noreply.github.com>
2022-07-14 15:32:48 +0530

    Add files via upload

---
+++ public/service-worker.js
@@ -0,0 +1,13 @@
+self.skipWaiting();
+
+self.addEventListener("fetch", function (_event) {
+  //nothing for now
+});
+    
+// self.addEventListener('install', event => {
+//     // Do something after install
+//   });
+
+//   self.addEventListener('activate', event => {
+//     // Do something upon activation
+//   });
